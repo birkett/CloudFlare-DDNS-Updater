a065fb0ccb476ee13d1511637e354c0ac9ffce58
Anthony Birkett <anthony@a-birkett.com>
2015-03-18 12:54:38 +0000

    Added basic service functionality. This will eventually allow the program to launch as a GUI, or as a service for updates.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.ServiceProcess;
 
 namespace CloudFlare_DDNS
 {
@@ -9,11 +10,38 @@ namespace CloudFlare_DDNS
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                if (args[0] == "-service")
+                {
+                    RunService();
+                    return;
+                }
+            }
+
+            RunGUI();
+            return;
+        }
+
+        static void RunGUI()
+        {
+            Logger.log("Starting CloudFlare DDNS updater GUI", Logger.Level.Info);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+        }
+
+        static void RunService()
+        {
+            Logger.log("Starting CloudFlare DDNS updater service", Logger.Level.Info);
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service() 
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
---
+++ Service/Service.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudFlare_DDNS
+{
+    /// <summary>
+    /// Run the update process as a Windows Service
+    /// </summary>
+    public partial class Service : ServiceBase
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Service startup
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Service stop
+        /// </summary>
+        protected override void OnStop()
+        {
+        }
+    }
+}
